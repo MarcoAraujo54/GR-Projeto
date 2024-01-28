d1c195e3b3d3025c67ea9d1c3a3b517d48715225
peleja23 <pedro_peleja@hotmail.com>
2024-01-28 00:08:57 +0000

    config update

---
+++ src/ConfigLoad.java
@@ -0,0 +1,49 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class ConfigLoad {
+    private String M;
+    private int K;
+    private int T;
+    private int V; // Validade
+    private int X; // Máximo de entradas na tabela
+
+    public void loadConfig(SnmpKeysMib mib){
+        try {
+            File file = new File("config.txt");
+            Scanner scanner = new Scanner(file);            
+            for (int i = 0; i < 7 && scanner.hasNextLine(); i++) {
+                String line = scanner.nextLine();
+                if (i == 2) {
+                    M = line;
+                    System.out.println(M);
+                }
+                else if(i == 3){
+                    K = Integer.parseInt(line.trim());
+                    System.out.println(K);
+                }
+                else if(i == 4){
+                    T = Integer.parseInt(line.trim());
+                    System.out.println(T);
+                }
+                else if(i == 5){
+                    V = Integer.parseInt(line.trim());
+                    System.out.println(V);
+                }
+                else if(i == 6){
+                    X = Integer.parseInt(line.trim());
+                    System.out.println(X);
+                }
+            }
+            scanner.close();
+        } 
+        catch (FileNotFoundException e) {
+            System.out.println("An error occurred: " + e.getMessage());
+
+        }
+        mib.getOids().put( "1.3",K);
+        mib.getOids().put( "1.4",T);
+        mib.getOids().put( "2.1",M);
+    }
+}
